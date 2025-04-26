a16d9346366ff857ae1a6b14a92ba0b2526d1bef
Nakhun12310 <163088668+Nakhun12310@users.noreply.github.com>
2025-04-26 19:36:37 +0700

    Update Loader.lua.js

--- api/Loader.lua.js
+++ api/Loader.lua.js
@@ -2,6 +2,6 @@ export default function handler(req, res) {
   res.setHeader('Content-Type', 'text/plain');
   res.status(200).send(`
 print("Script loaded!")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Nakhun12310/CookieHub/main/OldFluentFisch.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CookieHubScript/CookieLoader/main/Script.lua"))()
   `);
 }
