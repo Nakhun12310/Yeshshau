705ad1b13f407dfda2df74f38757059b2bed2467
Nakhun12310 <163088668+Nakhun12310@users.noreply.github.com>
2025-04-27 10:33:27 +0700

    Update script.lua

--- public/script.lua
+++ public/script.lua
@@ -1,2 +1,18 @@
-script_key = "PASTEKEYHERE";
-(loadstring or load)(game:HttpGet("https://getrealcookiehubscript.vercel.app/api/Loader.lua"))()
+local HttpService = game:GetService("HttpService")
+local Player = game.Players.LocalPlayer
+
+-- YOUR KEY
+local script_key = "PASTEKEYHERE"
+
+-- Check the key
+local success, result = pcall(function()
+    return HttpService:JSONDecode(game:HttpPost("https://getrealcookiehubscript.vercel.app/api/key.js", HttpService:JSONEncode({key = script_key}), Enum.HttpContentType.ApplicationJson, false))
+end)
+
+if success and result.valid then
+    -- Key valid, load the hub
+    (loadstring or load)(game:HttpGet("https://getrealcookiehubscript.vercel.app/api/Loader.lua"))()
+else
+    -- Key invalid or expired
+    Player:Kick("Invalid or expired key. Please get a new key!")
+end
