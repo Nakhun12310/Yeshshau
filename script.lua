724487297202769eeaddf873a64bc97c3a178823
Nakhun12310 <163088668+Nakhun12310@users.noreply.github.com>
2025-04-27 15:21:01 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Nakhun12310/CookieHub/refs/heads/main/OldFluentFisch.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nakhun12310/gommemomcook/refs/heads/main/OldFluentFisch.lua"))()
 print("HEY! ASS HOLE WHAT ARE YOU DOING -COOKIE HUB")
